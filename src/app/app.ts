dacd64475a12bd84dc82872e7a89f6d555118064
DESKTOP-4LMGJBN\ANUPRIYA <anupriyan.ai@gmail.com>
2026-09-01 13:16:57 +0530

    Add frontend developer portfolio

---
+++ src/app/app.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+import { Navbar } from './components/navbar/navbar';
+import { Home } from './components/home/home';
+import { About } from './components/about/about';
+import { Skills } from './components/skills/skills';
+import { Experience } from './components/experience/experience';
+import { Projects } from './components/projects/projects';
+import { Certifications } from './components/certifications/certifications';
+import { Contact } from './components/contact/contact';
+import { Footer } from './components/footer/footer';
+
+@Component({
+  selector: 'app-root',
+
+  standalone: true,
+
+  imports: [
+    Navbar,
+    Home,
+    About,
+    Skills,
+    Experience,
+    Projects,
+    Certifications,
+    Contact,
+    Footer
+  ],
+
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {}
